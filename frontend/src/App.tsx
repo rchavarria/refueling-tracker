187b836743ee96298cfaf8d942923e1e9452f438
Rubén Chavarría <rchavarria@easyvista.com>
2026-03-10 17:17:39 +0100

    Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Layout from "./components/Layout";
 import DashboardPage from "./pages/DashboardPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import RefuelingNewPage from "./pages/RefuelingNewPage";
+import ReminderEditPage from "./pages/ReminderEditPage";
+import ReminderNewPage from "./pages/ReminderNewPage";
 import VehicleDetailPage from "./pages/VehicleDetailPage";
 import VehicleListPage from "./pages/VehicleListPage";
 import VehicleNewPage from "./pages/VehicleNewPage";
@@ -17,6 +19,8 @@ export default function App() {
           <Route path="/vehicles/new" element={<VehicleNewPage />} />
           <Route path="/vehicles/:id" element={<VehicleDetailPage />} />
           <Route path="/vehicles/:id/refuelings/new" element={<RefuelingNewPage />} />
+          <Route path="/vehicles/:id/reminders/new" element={<ReminderNewPage />} />
+          <Route path="/vehicles/:id/reminders/:reminderId/edit" element={<ReminderEditPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
