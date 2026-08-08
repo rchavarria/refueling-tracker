4db03ffce137bbffc2ee003c238e88c639a7af48
Rubén Chavarría <rchavarria@easyvista.com>
2026-08-08 18:44:49 +0200

    Comment code to avoid using debug database

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-echo ""
-echo "#1 => Running database migrations..."
-npx prisma migrate deploy
-
-echo ""
-echo "#2 => Generating prisma client... (needed to seed db and run the backend)"
-npm run prisma:generate
-
-echo ""
-echo "#3 => Seeding database..."
-npm run db:seed
+#echo ""
+#echo "#1 => Running database migrations..."
+#npx prisma migrate deploy
+#
+#echo ""
+#echo "#2 => Generating prisma client... (needed to seed db and run the backend)"
+#npm run prisma:generate
+#
+#echo ""
+#echo "#3 => Seeding database..."
+#npm run db:seed
 
 echo ""
 echo "#4 => Starting backend server..."
